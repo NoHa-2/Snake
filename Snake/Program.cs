bbdf8e02a46574259b8ab6bfba2857ed1544e539
NoHa <gamerstroveirl@gmail.com>
2025-11-12 16:33:07 +0800

    Add project files.

---
+++ Snake/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.BufferWidth = 120;
+            Console.BufferHeight = 80;
+
+            while (true)
+            {
+                Update();
+            }
+        }
+
+        static void Update()
+        {
+            for (int i = 0; i < Console.BufferWidth; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write("=");
+                Console.SetCursorPosition(i, 5);
+                Console.Write("=");
+            }
+        }
+    }
+}
