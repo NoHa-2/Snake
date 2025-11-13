e36fd4bcf842da7852ff645caff842c93ad150f2
NoHa <gamerstroveirl@gmail.com>
2025-11-13 16:59:59 +0800

    snake class added

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,26 +6,49 @@ namespace Snake
 {
     class Program
     {
+        static int screenWidth = 120;
+        static int screenHeight = 80;
+        static List<Snek> sneke = new();
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
-            Console.BufferWidth = 120;
-            Console.BufferHeight = 80;
+            MakeSnek();
 
+            for (int i = 0; i < sneke.Count; i++)
+            {
+                Console.SetCursorPosition(sneke[i].x, sneke[i].y);
+                Console.Write("x");
+            }
+
+            // game loop
             while (true)
             {
+                // timing and input
+
+                // game logic
+
+                // display stuff
                 Update();
             }
         }
 
         static void Update()
         {
-            for (int i = 0; i < Console.BufferWidth; i++)
+            for (int i = 0; i < screenWidth; i++)
             {
                 Console.SetCursorPosition(i, 0);
                 Console.Write("=");
                 Console.SetCursorPosition(i, 5);
                 Console.Write("=");
+            }
+        }
+        public static void MakeSnek()
+        {
+            int x = 60;
+            int y = 15;
+            for (int i = 0; i <= 10; i++)
+            {
+                sneke.Add(new Snek(x + i, y));
             }
         }
     }
---
+++ Snake/snek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snek
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Snek(int xCoord, int yCoord)
+        {
+            x = xCoord;
+            y = yCoord;
+        }
+    }
+}
